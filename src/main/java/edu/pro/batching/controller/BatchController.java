62a2241c4c7361f2f79123721ea66e8d637599f6
gprohorov <george.prokhorov@external.getneteurope.com>
2022-04-18 23:37:02 +0300

    controller

---
+++ src/main/java/edu/pro/batching/controller/BatchController.java
@@ -0,0 +1,43 @@
+package edu.pro.batching.controller;
+/*
+  @author   george
+  @project   batching
+  @class  BatchController
+  @version  1.0.0 
+  @since 18.04.22 - 23.17
+*/
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/batch")
+public class BatchController {
+    @Autowired
+    private JobLauncher jobLauncher;
+    @Autowired
+    private Job job;
+    @GetMapping(path = "/start")
+    public void startBatch() {
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addLong("startAt", System.currentTimeMillis()).toJobParameters();
+        try {
+            jobLauncher.run(job, jobParameters);
+        } catch (JobExecutionAlreadyRunningException | JobRestartException
+                | JobInstanceAlreadyCompleteException | JobParametersInvalidException e) {
+
+            e.printStackTrace();
+        }
+    }
+}
